8e74934192d3530fc681d677fca0e37e477d7d2f
Tima test <timmy12122022@gmail.com>
2023-07-12 21:03:16 +0700

    initial commit

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -9,10 +9,24 @@ class MaxServiceTest {
     public void shouldFindMax() {
         MaxService service = new MaxService();
 
-        int a =5;
-        int b =3;
+        int a = 5;
+        int b = 3;
 
         int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfb() {
+
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
